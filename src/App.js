4f5ebf33bc7a050b4a3d9a95ef3e9bb9b96bece5
Muhammed Shahzad <muhammed.shahzad@vodafone.com>
2023-09-29 10:19:40 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and aws
+          Learn React and aws and here goes continuous deployment
         </a>
       </header>
     </div>
